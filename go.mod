1cbb0935b0ec7a79621084504767c668adf326af
Miraç <88139688+westydev@users.noreply.github.com>
2023-12-30 00:26:24 +0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/westydev/golibs
+
+go 1.21
---
+++ utils/cmd.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func Clear() {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}

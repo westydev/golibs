4bb60fee33252a26f596db8de1f19ee2a195ce29
Miraç <88139688+westydev@users.noreply.github.com>
2023-12-30 21:59:53 +0400

    Update cmd.go

--- utils/cmd.go
+++ utils/cmd.go
@@ -6,6 +6,12 @@ import (
 	"runtime"
 )
 
+func SetConsoleTitle(newTitle string) {
+	cmd := exec.Command("cmd", "/c", "title", newTitle)
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
 func Clear() {
 	var cmd *exec.Cmd
 
